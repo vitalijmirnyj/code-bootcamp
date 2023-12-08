99bef4ec739e0e3f03ab3ddb5bd0476f0fd6d358
vitalijmirnyj <mvitalijus@yahoo.com>
2023-12-08 15:14:10 +0200

    Complete excercise DivisibleByThree

---
+++ Java/part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -0,0 +1,15 @@
+
+public class DivisibleByThree {
+
+    public static void main(String[] args) {
+        divisibleByThreeInRange(5, 15);
+    }
+
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        for (int i = beginning; i <= end; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
