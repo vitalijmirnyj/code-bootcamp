7ce9b480193b7c8f6450f98ef3e707a209ee9df0
vitalijmirnyj <mvitalijus@yahoo.com>
2023-12-08 14:44:29 +0200

    Complete excercise Reprint

--- Java/part02-Part02_21.InAHoleInTheGround/src/main/java/InAHoleInTheGround.java
+++ Java/part02-Part02_21.InAHoleInTheGround/src/main/java/InAHoleInTheGround.java
@@ -8,4 +8,3 @@ public class InAHoleInTheGround {
         System.out.println("In a hole in the ground there lived a method");
     }
 }
-    
---
+++ Java/part02-Part02_22.Reprint/src/main/java/Reprint.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class Reprint {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many times?");
+        int howManyTimes = Integer.valueOf(scanner.nextLine());
+        for (int i = 1; i <= howManyTimes; i++) {
+            printText();
+        }
+    }
+    
+    // Don't change the next line that defines the method!
+    // (We aren't giving a method a parameter yet)
+    public static void printText() {
+        System.out.println("In a hole in the ground there lived a method");
+    }
+}
