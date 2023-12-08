cf9edfaec44b67a4ec52f2e28810ae792cb99af7
vitalijmirnyj <mvitalijus@yahoo.com>
2023-12-08 16:35:39 +0200

    Delete else statement

---
+++ Java/part02-Part02_31.Greatest/src/main/java/Greatest.java
@@ -0,0 +1,12 @@
+
+public class Greatest {
+
+    public static int greatest(int number1, int number2, int number3) {
+        //write some code here
+    }
+
+    public static void main(String[] args) {
+        int result = greatest(2, 7, 3);
+        System.out.println("Greatest: " + result);
+    }
+}
