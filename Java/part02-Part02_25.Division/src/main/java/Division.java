8c401d8f2c983b3d367d69244a5d74d016987e00
vitalijmirnyj <mvitalijus@yahoo.com>
2023-12-08 15:06:41 +0200

    Complete excercise

---
+++ Java/part02-Part02_25.Division/src/main/java/Division.java
@@ -0,0 +1,13 @@
+
+
+public class Division {
+
+    public static void main(String[] args) {
+
+        division(4, 5);
+
+    }
+    public static void division ( int numerator, int denominator){
+        System.out.println((double) numerator / denominator);
+    }
+}
