880b610452250217b0caacf04fbdd411f11409b9
vitalijmirnyj <mvitalijus@yahoo.com>
2023-12-08 12:35:33 +0200

    Complete excercise Repeating Breaking

---
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+public class RepeatingBreakingAndRemembering {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give numbers:");
+        int sum = 0;
+        int numberOfNumbers = 0;
+        int evenNumbers = 0;
+        int oddNumbers = 0;
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+
+            if (number != -1) {
+                sum = sum + number;
+                numberOfNumbers = numberOfNumbers + 1;
+
+            }
+            if (number % 2 == 0) {
+                evenNumbers = evenNumbers + 1;
+
+            } else if (number != -1) {
+                oddNumbers = oddNumbers + 1;
+            }
+            if (number == -1) {
+                break;
+            }
+        }
+        System.out.println("Thx! Bye!");
+        System.out.println("Sum: " + sum);
+        System.out.println("Numbers: " + numberOfNumbers);
+        System.out.println("Average: " + ((double)sum / numberOfNumbers));
+        System.out.println("Even: " + evenNumbers );
+        System.out.println("Odd: " + oddNumbers);
+    }
+}
