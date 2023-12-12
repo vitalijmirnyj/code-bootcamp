918765a5b428ff603da8f61aa9afef729ee1be1f
vitalijmirnyj <goforitasvm@gmail.com>
2023-12-12 13:00:29 +0200

    Complete tasks: GreatestInList, IndexOf

---
+++ Java/Lists/part03-Part03_09.GreatestInList/src/main/java/GreatestInList.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GreatestInList {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input == -1) {
+                break;
+            }
+
+            list.add(input);
+        }
+        
+        System.out.println("");
+
+        int greatestNumber = list.get(0);
+        for (int i = 1; i <= list.size() - 1; i++) {
+            if (list.get(i) > greatestNumber) {
+                greatestNumber = list.get(i);
+            }
+        }
+        System.out.println("The greatest number: " + greatestNumber);
+    }
+}
