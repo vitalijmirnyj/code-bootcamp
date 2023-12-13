5dabd4c090802e41cb3a3738cfefd0b93936dba3
vitalijmirnyj <goforitasvm@gmail.com>
2023-12-13 09:13:40 +0200

    Complete task: RemoveLast

---
+++ Java/Lists/part03-Part03_17.RemoveLast/src/main/java/RemoveLast.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+
+public class RemoveLast {
+
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+
+        System.out.println(strings);
+        removeLast(strings);
+    }
+
+    public static void removeLast(ArrayList<String> strings) {
+        if (strings.size() < 1) {
+            return;
+        }
+        strings.remove(strings.size() - 1);
+        System.out.println(strings);
+    }
+}
