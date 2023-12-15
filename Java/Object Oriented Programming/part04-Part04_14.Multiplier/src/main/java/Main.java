5f031581685a19f77afee134dc28fc423900191a
vitalijmirnyj <goforitasvm@gmail.com>
2023-12-15 14:34:25 +0200

    Complete task: 14.Multiplier

---
+++ Java/Object Oriented Programming/part04-Part04_14.Multiplier/src/main/java/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        Multiplier multiply = new Multiplier(5);
+        System.out.println("The number is " + multiply.multiply(2));
+
+    }
+}
