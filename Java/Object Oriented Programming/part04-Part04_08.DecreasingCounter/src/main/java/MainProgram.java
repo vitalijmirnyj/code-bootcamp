6d7e5a0de71291fda3c9be5674932b5a46ec22e6
vitalijmirnyj <goforitasvm@gmail.com>
2023-12-15 12:39:42 +0200

    Complete task: 08.DecreasingCounter

---
+++ Java/Object Oriented Programming/part04-Part04_08.DecreasingCounter/src/main/java/MainProgram.java
@@ -0,0 +1,11 @@
+
+public class MainProgram {
+
+
+    public static void main(String[] args) {
+
+        DecreasingCounter decreasingCounter = new DecreasingCounter(0);
+            decreasingCounter.decrement();
+        decreasingCounter.printValue();
+    }
+}
