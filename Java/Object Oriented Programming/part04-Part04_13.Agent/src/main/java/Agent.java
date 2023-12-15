ca008cbdbf98739ffac49b2daa83d5c0aac5adad
vitalijmirnyj <goforitasvm@gmail.com>
2023-12-15 14:46:07 +0200

    Modify task: 13.Agent

--- Java/Object Oriented Programming/part04-Part04_13.Agent/src/main/java/Agent.java
+++ Java/Object Oriented Programming/part04-Part04_13.Agent/src/main/java/Agent.java
@@ -1,5 +1,5 @@
 
-public class    Agent {
+public class Agent {
 
     private String firstName;
     private String lastName;
@@ -10,7 +10,7 @@ public class    Agent {
     }
     
     public String toString() {
-        return "My name is " + this.lastName + ", " + this.firstName + " " + this.lastName;
+        return "My name is " + lastName + ", " + firstName + " " + lastName;
     }
     
 }
---
+++ Java/Object Oriented Programming/part04-Part04_13.Agent/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+       Agent agent = new Agent("James", "Bond");
+        System.out.println(agent.toString());
+    }
+}
