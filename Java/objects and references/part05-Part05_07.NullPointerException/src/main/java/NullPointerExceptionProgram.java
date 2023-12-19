b91f9ee048319d4026069d76f1ddf049f0275dfc
vitalijmirnyj <goforitasvm@gmail.com>
2023-12-19 16:27:02 +0200

    Complete task: 07.NullPointerException

---
+++ Java/objects and references/part05-Part05_07.NullPointerException/src/main/java/NullPointerExceptionProgram.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class NullPointerExceptionProgram {
+
+    public static void main(String[] args) {
+      String name = null;
+        System.out.println(name.toUpperCase());
+    }
+
+}
