2f6075da0ff16c44f2a16342e44b00f85f28433c
vitalijmirnyj <goforitasvm@gmail.com>
2023-12-20 15:53:45 +0200

    Complete task: 13.IdenticalTwins

---
+++ Java/objects and references/part05-Part05_13.IdenticalTwins/src/main/java/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // you can write code here to try out your program
+
+    }
+}
---
+++ Java/objects and references/part05-Part05_13.IdenticalTwins/src/main/java/Person.java
@@ -0,0 +1,35 @@
+
+import java.util.Objects;
+
+public class Person {
+
+    private String name;
+    private SimpleDate birthday;
+    private int height;
+    private int weight;
+
+    public Person(String name, SimpleDate birthday, int height, int weight) {
+        this.name = name;
+        this.birthday = birthday;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public boolean equals(Object compared) {
+
+        if (this == compared) {
+            return true;
+        }
+
+        if (!(compared instanceof Person)) {
+            return false;
+        }
+
+        Person otherPerson = (Person) compared;
+
+        if (this.name == otherPerson.name && this.height == otherPerson.height && this.weight == otherPerson.weight && this.birthday.equals(otherPerson.birthday)) {
+            return true;
+        }
+        return false;
+    }
+}
