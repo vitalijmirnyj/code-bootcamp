5de37d4c8c0caa7820a63d8a45ee95f8e729ba45
vitalijmirnyj <goforitasvm@gmail.com>
2023-12-20 12:32:05 +0200

    Complete task11.ComparingApartments

---
+++ Java/objects and references/part05-Part05_11.ComparingApartments/src/main/java/Apartment.java
@@ -0,0 +1,38 @@
+public class Apartment {
+    private int rooms;
+    private int squares;
+    private int pricePerSquare;
+
+    public Apartment(int rooms, int squares, int pricePerSquare) {
+        this.rooms = rooms;
+        this.squares = squares;
+        this.pricePerSquare = pricePerSquare;
+    }
+
+    public boolean largerThan(Apartment compared) {
+
+        if (this.squares > compared.squares) {
+            return true;
+
+        } else {
+            return false;
+        }
+    }
+
+    public int priceDifference(Apartment compared) {
+
+        int firstApartmentPrice = this.pricePerSquare * this.squares;
+        int comparedApartmentPrice = compared.pricePerSquare * compared.squares;
+        return Math.abs(firstApartmentPrice - comparedApartmentPrice);
+    }
+
+    public boolean moreExpensiveThan(Apartment compared) {
+
+        if (this.pricePerSquare > compared.pricePerSquare) {
+            return true;
+            
+        } else {
+            return false;
+        }
+    }
+}
