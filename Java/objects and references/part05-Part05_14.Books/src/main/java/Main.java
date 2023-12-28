f75cce249ff861ccd2b855db07b7501a85638961
vitalijmirnyj <goforitasvm@gmail.com>
2023-12-28 14:48:35 +0200

    Create new HTML folder for html tasks and one index file with task 1

---
+++ Java/objects and references/part05-Part05_14.Books/src/main/java/Main.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // NB! Don't alter the line below!
+        System.out.println("Thank you! Books added: " + books.size());
+    }
+}
