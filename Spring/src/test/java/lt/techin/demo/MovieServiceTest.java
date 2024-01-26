fe0e52f8cab55f27d04afe12e7b18fa0d843e2ac
vitalijmirnyj <goforitasvm@gmail.com>
2024-01-26 10:04:26 +0200

    Add method using containsExactly in test

--- Spring/src/test/java/lt/techin/demo/MovieServiceTest.java
+++ Spring/src/test/java/lt/techin/demo/MovieServiceTest.java
@@ -33,15 +33,7 @@ public class MovieServiceTest {
         Movie savedMovie1 = this.movieRepository.save(new Movie("Toy Story 3", "John Malkovich", (short) 1995, (short) 120));
         Movie savedMovie2 = this.movieRepository.save(new Movie("Toy Story 4", "John M", (short) 1995, (short) 120));
         List<Movie> movies = this.movieService.findAllMovies();
-        then(movies.get(0).getTitle()).isEqualTo(savedMovie1.getTitle());
-        then(movies.get(0).getDirector()).isEqualTo(savedMovie1.getDirector());
-        then(movies.get(0).getYearRelease()).isEqualTo(savedMovie1.getYearRelease());
-        then(movies.get(0).getLengthMinutes()).isEqualTo(savedMovie1.getLengthMinutes());
-
-        then(movies.get(1).getTitle()).isEqualTo(savedMovie2.getTitle());
-        then(movies.get(1).getDirector()).isEqualTo(savedMovie2.getDirector());
-        then(movies.get(1).getYearRelease()).isEqualTo(savedMovie2.getYearRelease());
-        then(movies.get(1).getLengthMinutes()).isEqualTo(savedMovie2.getLengthMinutes());
+        then(movies).containsExactly(savedMovie1, savedMovie2);
     }
 
 }
