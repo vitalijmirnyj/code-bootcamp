b51ec66620e93f9485447dc1a8316d6126aa568c
vitalijmirnyj <goforitasvm@gmail.com>
2024-01-26 11:58:09 +0200

    Add findMovieById_findNotExistent_throwError test

--- Spring/src/test/java/lt/techin/demo/MovieServiceTest.java
+++ Spring/src/test/java/lt/techin/demo/MovieServiceTest.java
@@ -13,9 +13,11 @@ import org.assertj.core.api.BDDAssumptions;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.catchThrowable;
 import static org.assertj.core.api.BDDAssertions.then;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
@@ -33,6 +35,12 @@ public class MovieServiceTest {
         Movie savedMovie1 = this.movieRepository.save(new Movie("Toy Story 3", "John Malkovich", (short) 1995, (short) 120));
         Movie foundMovie = this.movieService.findMovieById(savedMovie1.getId());
         then(foundMovie).isEqualTo(savedMovie1);
+    }
+
+    @Test
+    void findMovieById_findNotExistent_throwError() {
+        Throwable throwable = catchThrowable(() -> this.movieService.findMovieById(1));
+        then(throwable).isInstanceOf(NoSuchElementException.class);
     }
 
 }
