3c57f2543e8147798dead825d5f395b5be219dd5
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-01 12:53:18 +0200

    Modify code testing positive(numbers) method and align code

--- Java/Handling collections as streams/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ Java/Handling collections as streams/part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -8,12 +8,18 @@ public class PositiveNumbers {
 
     public static void main(String[] args) {
         List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(-3);
+        numbers.add(2);
+        numbers.add(-6);
+
+        List<Integer> positiveNumbers = positive(numbers);
+
+        System.out.println("Positive Numbers: " + positiveNumbers);
     }
 
     public static List<Integer> positive(List<Integer> numbers) {
-        return numbers.stream()
-                .filter(num -> num > 0)
-                .collect(Collectors.toCollection(ArrayList::new));
+        return numbers.stream().filter(num -> num > 0).collect(Collectors.toCollection(ArrayList::new));
     }
 }
 
