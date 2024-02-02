4e509fc479c175a02f673365a854e18a0f2a5571
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-02 13:10:02 +0200

    Create User class

---
+++ Spring/src/main/java/lt/techin/demo/models/User.java
@@ -0,0 +1,52 @@
+package lt.techin.demo.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    @NotNull
+    @Size(min = 6, message = "User name should be at least 6 characters long")
+    private String username;
+    @Size(min = 8, message = "Password should be at least 8 characters long")
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
