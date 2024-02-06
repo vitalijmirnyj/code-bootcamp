e1b6cbc26beffb6d0596d27e039cfd80f22b89e6
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-06 11:42:58 +0200

    Add @EnableWebSecurity annotation

--- Spring/src/main/java/lt/techin/demo/Security/SecurityConfig.java
+++ Spring/src/main/java/lt/techin/demo/Security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -18,6 +19,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
