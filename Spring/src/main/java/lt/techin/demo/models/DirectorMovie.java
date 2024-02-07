77e8e54ee9b34e39a8745facef138da0bc11ba43
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-07 09:47:04 +0200

    Complete DirectorMovie class

--- Spring/src/main/java/lt/techin/demo/models/DirectorMovie.java
+++ Spring/src/main/java/lt/techin/demo/models/DirectorMovie.java
@@ -12,6 +12,26 @@ import java.io.Serializable;
 public class DirectorMovie {
 
     @EmbeddedId
+    private DirectorMovieId directorMovieId;
+
+    private String role;
+
+    public DirectorMovieId getDirectorMovieId() {
+        return directorMovieId;
+    }
+
+    public void setDirectorMovieId(DirectorMovieId directorMovieId) {
+        this.directorMovieId = directorMovieId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
 
 
 }
