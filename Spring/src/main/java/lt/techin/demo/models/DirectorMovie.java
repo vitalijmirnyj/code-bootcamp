de2d70f0c3020104582be610426dd5889c2e9907
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-07 09:40:44 +0200

    Add DirectorMovieId class

--- Spring/src/main/java/lt/techin/demo/models/DirectorMovie.java
+++ Spring/src/main/java/lt/techin/demo/models/DirectorMovie.java
@@ -1,4 +1,17 @@
 package lt.techin.demo.models;
 
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Directors_movies")
 public class DirectorMovie {
+
+    @EmbeddedId
+
+
 }
