ae9d7428a08799a66b0420cbc48e7cdaf746f1db
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-07 13:56:45 +0200

    Delete bad code DirectorMovieRepository

--- Spring/src/main/java/lt/techin/demo/repositories/DirectorMovieRepository.java
+++ Spring/src/main/java/lt/techin/demo/repositories/DirectorMovieRepository.java
@@ -8,12 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface DirectorMovieRepository extends JpaRepository<DirectorMovie, DirectorMovieId> {
-    Optional<DirectorMovie> findByDirectorIdAndMovieId(long directorId, long movieId);
 
-    void deleteByDirectorIdAndMovieId(long directorId, long movieId);
-
-    void existsDerictorMovieById(long directorId, long movieId);
-
-    boolean existsById(long directorId, long movieId);
 }
 
