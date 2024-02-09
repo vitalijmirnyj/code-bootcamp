d2f353a2a13944e22d57d7c15816add7a2c53a78
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-09 15:46:18 +0200

    Add Ternary operator solution to task 4

--- Javascript/Praktika_1/script.js
+++ Javascript/Praktika_1/script.js
@@ -36,15 +36,19 @@ function EvenOrOdd(number) {
 }
 
 //.4
-let number1;
-let number2;
-
+//Conditional
 function findBiggerOfTwoNumbers(number1, number2) {
   if (number1 > number2 && number1 != number2) {
     return number1;
   } else {
     return number2;
   }
+}
+
+// Ternary operator
+function findBiggerOfTwoNumbers(number1, number2) {
+  let biggerNumber = number1 > number2 ? number1 : number2;
+  console.log(biggerNumber);
 }
 
 //5.
