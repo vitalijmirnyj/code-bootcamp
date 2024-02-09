559f0c6c4b6d81276ba2f334835891fc0d72bcff
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-09 15:28:58 +0200

    Complete task 5

--- Javascript/Praktika_1/script.js
+++ Javascript/Praktika_1/script.js
@@ -48,6 +48,21 @@ function findBiggerOfTwoNumbers(number1, number2) {
 }
 
 //5.
+function grade(points) {
+  if (points <= 29) {
+    console.log("Failed");
+  } else if (points > 29 && points <= 34) {
+    console.log("Your Grade is 1");
+  } else if (points > 34 && points <= 39) {
+    console.log("Your Grade is 2");
+  } else if (points > 39 && points <= 44) {
+    console.log("Your Grade is 3");
+  } else if (points > 44 && points <= 49) {
+    console.log("Your Grade is 4");
+  } else {
+    console.log("Your Grade is 5");
+  }
+}
 
 //6.
 
