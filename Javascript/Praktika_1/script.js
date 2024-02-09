7a66b9c085c83a9d499998749f2e1c0f34de4aa9
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-09 14:59:57 +0200

    Complete task 9 divisible from 5

--- Javascript/Praktika_1/script.js
+++ Javascript/Praktika_1/script.js
@@ -110,3 +110,12 @@ function timeOfYear(monthNumber) {
       break;
   }
 }
+
+//9.
+function everyNumberDivisibleFromFive() {
+  for (let i = 0; i < 1000; i++) {
+    if (i % 5 == 0) {
+      console.log(i);
+    }
+  }
+}
