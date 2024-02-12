d1f2537dc4460568369af9cacc05158766fa842a
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-12 11:06:11 +0200

    Complete Praktika_0

---
+++ Javascript/Praktika_0/js/javascript.js
@@ -0,0 +1,22 @@
+const image = document.querySelector(
+  "a.navbar-brand:nth-child(2) > img:nth-child(1)"
+);
+console.log(image);
+
+const h1 = document.querySelector("h1");
+console.log(h1);
+
+const elements = document.querySelector(".hero-info");
+const thirdElement = elements.getElementsByClassName("bubb");
+console.log(thirdElement);
+
+const navClass = document.querySelector(".lnr-magnifier");
+console.log(navClass);
+
+const navItem = document.querySelector(
+  "div.flex-lg-row:nth-child(1) > li:nth-child(2) > a:nth-child(1)"
+);
+console.log(navItem);
+
+const button = document.querySelector("a.btn:nth-child(1)");
+console.log(button);
