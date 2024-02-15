969702b1ceb49722d76f7fba9c3e44fc9628811e
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-15 15:14:45 +0200

    Modify Praktika_1 to use ternary operator

--- React/hooks/Praktika_1/Tema_2_Hooks/src/components/Card.jsx
+++ React/hooks/Praktika_1/Tema_2_Hooks/src/components/Card.jsx
@@ -2,40 +2,29 @@ import "./Card.css";
 import { useState } from "react";
 
 export default function Card() {
-  const [text, setText] = useState("Task is not done!");
-  const [btnText, setBtnText] = useState("Mark as done");
-  const [btnColor, setBtnColor] = useState("red");
+  const [btnPressed, setBtnPressed] = useState(false);
+  function changeBtnPressed() {
+    setBtnPressed(!btnPressed);
+  }
 
-  const changeText = () => {
-    setText("Task is done!");
-  };
-  const changebtnText = () => {
-    setBtnText("Done");
-  };
-  const changeBtnColor = () => {
-    setBtnColor("green");
-  };
   return (
     <div
       className="card"
       style={{ width: "18rem" }}
     >
       <div className="card-body ">
-        <h5 className="card-title">{text}</h5>
+        <h5 className="card-title">
+          {btnPressed ? "Task is done!" : "Task is not done!"}
+        </h5>
         <p className="card-text">
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </p>
         <button
-          className="btn"
-          onClick={() => {
-            changeText();
-            changebtnText();
-            changeBtnColor();
-          }}
-          style={{ backgroundColor: btnColor }}
+          className={`btn ${btnPressed ? "btn-green" : ""}`}
+          onClick={changeBtnPressed}
         >
-          {btnText}
+          {btnPressed ? "Done" : "Mark as done"}
         </button>
       </div>
     </div>
