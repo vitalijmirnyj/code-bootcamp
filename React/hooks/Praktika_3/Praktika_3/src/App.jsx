0171095219b20a0f4405b07637d95b77da20e064
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-19 11:13:16 +0200

    Modify task: Praktika_3

--- React/hooks/Praktika_3/Praktika_3/src/App.jsx
+++ React/hooks/Praktika_3/Praktika_3/src/App.jsx
@@ -5,7 +5,12 @@ function App() {
   return (
     <>
       <div className="container border border-2">
-        <TextBox />
+        <TextBox
+          text="Focused, hard work is the real key. Achievement hinges on dedicated, diligent effort. Success is attained through concentrated, determined labor. The cornerstone of accomplishment is unwavering, industrious dedication. Focused and persistent work is the ultimate determinant of success. Without a doubt, focused, hard work remains paramount in any pursuit. It serves as the bedrock upon which success is built. Those who embrace this ethos invariably find themselves on the path to greatness. In the face of challenges, perseverance coupled with diligent effort becomes indispensable. Ultimately, it is the disciplined execution of tasks that separates the achievers from the dreamers."
+          maxLength={35}
+          text2="Winners embrace hard work. They love the challenge of pushing their limits and surpassing their goals. With each obstacle they overcome, their determination grows stronger. Every setback is seen as an opportunity to learn and improve. They understand that success is not just about talent, but about perseverance and resilience."
+          maxLength2={27}
+        />
       </div>
     </>
   );
--- React/hooks/Praktika_3/Praktika_3/src/components/TextBox.jsx
+++ React/hooks/Praktika_3/Praktika_3/src/components/TextBox.jsx
@@ -2,47 +2,35 @@ import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./TextBox.css";
 
-export default function textBox() {
-  let readMore = " ...read more";
-  const [textBox1, setTextBox1] = useState(
-    "Focused, hard work is the real key"
+export default function textBox({ text, maxLength, text2, maxLength2 }) {
+  const [displayText, setDisplayText] = useState(text.substring(0, maxLength));
+  const changeText = () => {
+    setDisplayText(text);
+  };
+  const [displayText2, setDisplayText2] = useState(
+    text2.substring(0, maxLength2)
   );
-  const [textBox2, setTextBox2] = useState(
-    "Winners embrace hard work. They love"
-  );
-  function changeTextBox1() {
-    setTextBox1(
-      "Focused, hard work is the real key. Achievement hinges on dedicated, diligent effort. Success is attained through concentrated, determined labor. The cornerstone of accomplishment is unwavering, industrious dedication. Focused and persistent work is the ultimate determinant of success. Without a doubt, focused, hard work remains paramount in any pursuit. It serves as the bedrock upon which success is built. Those who embrace this ethos invariably find themselves on the path to greatness. In the face of challenges, perseverance coupled with diligent effort becomes indispensable. Ultimately, it is the disciplined execution of tasks that separates the achievers from the dreamers."
-    );
-  }
-
-  function changeTextBox2() {
-    setTextBox2(
-      "Winners embrace hard work. They love challenges, striving relentlessly towards their goals. Dedication fuels their journey, propelling them forward. With each obstacle, they find opportunity to grow stronger. Their passion for excellence drives them to surpass expectations. In the pursuit of victory, hard work is their faithful companion."
-    );
-  }
+  const changeText2 = () => {
+    setDisplayText2(text2);
+  };
 
   return (
     <div>
-      <p>
-        {textBox1}
-        <span
-          className="custom-underline"
-          onClick={changeTextBox1}
-        >
-          {readMore}
-        </span>
-      </p>
-
-      <p>
-        {textBox2}
-        <span
-          className="custom-underline"
-          onClick={changeTextBox2}
-        >
-          {readMore}
-        </span>
-      </p>
+      {displayText}
+      <a
+        href="#"
+        onClick={changeText}
+      >
+        ...read more
+      </a>
+      <p></p>
+      {displayText2}
+      <a
+        href="#"
+        onClick={changeText2}
+      >
+        ...read more
+      </a>
     </div>
   );
 }
