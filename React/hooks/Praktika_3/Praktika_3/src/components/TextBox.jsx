355b2801bb2102d8d5d32cdc745fda06654906f4
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-19 11:21:13 +0200

    Modify task: Praktika_3

--- React/hooks/Praktika_3/Praktika_3/src/components/TextBox.jsx
+++ React/hooks/Praktika_3/Praktika_3/src/components/TextBox.jsx
@@ -2,16 +2,10 @@ import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./TextBox.css";
 
-export default function textBox({ text, maxLength, text2, maxLength2 }) {
+export default function textBox({ text, maxLength }) {
   const [displayText, setDisplayText] = useState(text.substring(0, maxLength));
   const changeText = () => {
     setDisplayText(text);
-  };
-  const [displayText2, setDisplayText2] = useState(
-    text2.substring(0, maxLength2)
-  );
-  const changeText2 = () => {
-    setDisplayText2(text2);
   };
 
   return (
@@ -20,14 +14,6 @@ export default function textBox({ text, maxLength, text2, maxLength2 }) {
       <a
         href="#"
         onClick={changeText}
-      >
-        ...read more
-      </a>
-      <p></p>
-      {displayText2}
-      <a
-        href="#"
-        onClick={changeText2}
       >
         ...read more
       </a>
