d0a8c962ffbd9ca1b0bb05108ab1ee1ffa331d0d
vitalijmirnyj <goforitasvm@gmail.com>
2024-02-21 14:23:30 +0200

    Add validation to post actor form fields

--- React/Tema_3 react forms/React Forms Praktika_1/src/components/MyForm.jsx
+++ React/Tema_3 react forms/React Forms Praktika_1/src/components/MyForm.jsx
@@ -32,18 +32,65 @@ export default function MyForm() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <p></p>
         Gender:
-        <input {...register("gender")} />
+        <input
+          {...register("gender", {
+            required: true,
+            minLength: 3,
+          })}
+        />
+        {errors?.gender?.type === "required" && <p>This field is required</p>}
+        {errors?.gender?.type === "minLength" && (
+          <p>Must be more than 3 characters</p>
+        )}
         Age:
         <input
           type="number"
-          {...register("age")}
+          {...register("age", {
+            required: true,
+            min: 1,
+            max: 125,
+          })}
         />
+        {errors?.age?.type === "required" && <p>This field is required</p>}
+        {errors?.age?.type === "min" && <p>Must be at least 1 year old</p>}
+        {errors?.age?.type === "max" && (
+          <p>Must be not older than 125 years old</p>
+        )}
         Nationality:
-        <input {...register("nationality")} />
+        <input
+          {...register("nationality", {
+            required: true,
+            minLength: 2,
+          })}
+        />
+        {errors?.nationality?.type === "required" && (
+          <p>This field is required</p>
+        )}
+        {errors?.nationality?.type === "minLength" && (
+          <p>Must be more than 3 characters</p>
+        )}
         Name:
-        <input {...register("name")} />
+        <input
+          {...register("name", {
+            required: true,
+            minLength: 2,
+          })}
+        />
+        {errors?.name?.type === "required" && <p>This field is required</p>}
+        {errors?.name?.type === "minLength" && (
+          <p>Must be more than 3 characters</p>
+        )}
         Surname:
-        <input {...register("surname")} />
+        <input
+          {...register("surname", {
+            required: true,
+            minLength: 3,
+          })}
+        />
+        {errors?.surname?.type === "required" && <p>This field is required</p>}
+        {errors?.surname?.type === "minLength" && (
+          <p>Must be more than 3 characters</p>
+        )}
         <input type="submit" />
       </form>
     </div>
